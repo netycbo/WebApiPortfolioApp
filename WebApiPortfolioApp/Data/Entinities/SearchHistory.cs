4a033cc727f08f7743bbdde01116bc987b09099a
netyc <netyc@MIKI>
2025-03-03 14:13:24 +0100

    refactor

--- WebApiPortfolioApp/Data/Entinities/SearchHistory.cs
+++ WebApiPortfolioApp/Data/Entinities/SearchHistory.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.Components.DictionaryAdapter;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using WebApiPortfolioApp.Data.Entinities.Identity;
@@ -6,11 +7,12 @@ using WebApiPortfolioApp.Data.Entinities.Identity;
 namespace WebApiPortfolioApp.Data.Entinities
 {
     [Table("SearchHistory")]
+    
     public class SearchHistory : AuditableEntity
     {
         public int Id { get; set; }
-        [MaxLength]
-        public string UserId { get; set; } 
+        [MaxLength(25)]
+        public string UserId { get; set; }
         public string SearchString { get; set; }
         public DateTime SearchDate { get; set; }
         public string Store { get; set; }
